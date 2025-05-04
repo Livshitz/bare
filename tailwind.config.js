0a5449ad1ce9270c2f75deefc422dc00e64973de
Elya Livshitz <elya.livshitz@gmail.com>
2025-05-05 01:28:37 +0300

    + Enhance Tailwind configuration to include CSS variables for colors * update LESS variables to use Tailwind's zinc color palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,25 @@ export default {
 		extend: {},
 	},
 	darkMode: 'class',
-	plugins: [require('daisyui')],
+	plugins: [
+		require('daisyui'),
+		function({ addBase, theme }) {
+			const colors = theme('colors');
+			const cssVars = {};
+			
+			Object.entries(colors).forEach(([colorName, colorValue]) => {
+				if (typeof colorValue === 'object') {
+					Object.entries(colorValue).forEach(([shade, value]) => {
+						cssVars[`--color-${colorName}-${shade}`] = value;
+					});
+				} else {
+					cssVars[`--color-${colorName}`] = colorValue;
+				}
+			});
+
+			addBase({
+				':root': cssVars,
+			});
+		},
+	],
 };
